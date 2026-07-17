d1d7d027cfed904ab737430b0a506006d7c8e30f
Daniel Bui <duykhanhb60@gmail.com>
2026-07-17 08:13:54 +0700

    fix: allow public brand assets

--- proxy.ts
+++ proxy.ts
@@ -39,5 +39,5 @@ export default auth((request) => {
 })
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"]
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|brand/).*)"]
 }
